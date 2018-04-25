60019099d7b39cdf17b643c63a911139beb61aa4
Popnararat <5710110222@psu.ac.th>
2018-04-25 14:05:08 +0700

    combined reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+let bearReducer = function(state = [], action){
+  if(action){
+    if(action.type == 'FETCH_BEAR'){
+      return action.payload
+    }
+  }
+  return state
+}
+let store = createStoreWithMiddleware(combineReducers({
+  bear: bearReducer
+}))
+
+let fetchBearActionCreator = function(){
+  return (dispatch) => {
+    axios.get('http://localhost:8000/api/bears')
+    .then(result => {
+      dispatch({type: 'FETCH_BEAR', payload: result.data})
+    })
+  }
+}
 
 class App extends Component {
   constructor(props){
@@ -7,10 +31,10 @@ class App extends Component {
     this.state = {data: []};
   }
   componentDidMount(){
-    axios.get('http://localhost:8000/api/bears')
-    .then(result => {
-      this.setState({data: result.data})
+    store.subscribe(() => {
+      this.setState({data: store.getState().bear})
     })
+    store.dispatch(fetchBearActionCreator());
   }
   render() {
     let bears = this.state.data;
