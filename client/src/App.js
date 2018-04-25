f55507d28ca939cbb7a1a41a1afa5fe99063f8ef
Popnararat <5710110222@psu.ac.th>
2018-04-25 22:00:48 +0700

    supprt create bear

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,34 @@
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { deleteBear } from './actions';
+import { deleteBear, createBear } from './actions';
 
-const renderBear = (bear) => (
-  <div key={bear.id}>
-    {bear.name}
-      <button>x</button>
-      </div>
-
-)
 class App extends Component {
   constructor(props){
     super(props)
-    
+    this.state = {bearName: ''};
   }
- 
+  onBearNameChanged = (e) => {
+    this.setState({bearName: e.target.value});
+  }
+  toCreateBear = () => {
+    this.props.createBear({name: this.state.bearName});
+    this.setState({bearName: ''});
+  }
+
   render() {
     let bears = this.props.bear;
     return (
       <div>
         {
-          bears.map(renderBear)
+          bears.map(bear => (
+            <div key={bear.id}>
+              {bear.name}
+              <button onClick={() => this.props.deleteBear(bear.id)}>X</button>
+            </div>
+          ))
         }
+        <input value={this.state.bearName} onChange={this.onBearNameChanged}/>
+        <button onClick={this.toCreateBear}>Add</button>
       </div>
     );
   }
@@ -31,4 +37,7 @@ class App extends Component {
 let mapStateToProps = (state) => (
   {bear: state.bear}
 )
-export default connect(mapStateToProps)(App);App;
+export default connect(mapStateToProps, {
+  deleteBear,
+  createBear,
+})(App);
